6ce5830c157d33244bdd6003f421cf2ff24485bc
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-06-17 20:27:56 -0500

    all files created and basic structure planned

---
+++ assets/JS/script.js
@@ -0,0 +1,29 @@
+// the questions array of objects
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____. ",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store _____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above "],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+];
+var counter = 60;
